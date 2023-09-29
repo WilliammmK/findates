1f601113d1f570b016887df3a7c41b69d46e5463
Guilherme Kobara <guilhermekobara@gmail.com>
2023-09-29 19:46:26 +0100

    Improving documentation and cleaning up comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
-//! # Findates
-//! A library for dates and date operations required by financial products.
+//! # Findates": A library for dates and date operations required by financial products.
 //! 
 //! Any basic calculation for financial products references a notion of time. Multiple conventions exist 
 //! so that computation of time in discrete periods can be achieved. While there are multiple resources for
